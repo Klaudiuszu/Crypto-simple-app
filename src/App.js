db6258095a2a3f70b36f890dddea845a69f5d63b
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2022-02-16 22:56:02 +0100

    install axios and set use states for api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-
 import './App.css';
+import {useEffect, useState} from 'react';
+import  axios from 'axios';
 
 function App() {
+  //reslove
+  const [data, setData] = useState([]);
+  //rejected
+  const [error, setError] = useState(null);
+  //pending
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="App">
     </div>
